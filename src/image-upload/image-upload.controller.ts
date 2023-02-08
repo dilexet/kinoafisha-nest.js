aa4f5ed4ec2a42516107f9c7f2c22123e86f397b
Pavarha Maksim <dilexet@gmail.com>
2023-02-08 14:53:05 +0300

    added get images

--- src/image-upload/image-upload.controller.ts
+++ src/image-upload/image-upload.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post, Res, UploadedFile, HttpStatus, BadRequestException } from '@nestjs/common';
+import { Controller, Post, Res, UploadedFile, HttpStatus, BadRequestException, Get } from '@nestjs/common';
 import { ApiFile } from './decorators/api-file.decorator';
 import { Response } from 'express';
 import { ApiTags } from '@nestjs/swagger';
+import { ImageUploadService } from './image-upload.service';
 
 @ApiTags('Image upload')
 @Controller('image-upload')
 export class ImageUploadController {
-  constructor() {
+  constructor(private readonly imageUploadService: ImageUploadService) {
   }
 
   @Post()
@@ -16,5 +17,11 @@ export class ImageUploadController {
       throw new BadRequestException('File is null');
     }
     return res.status(HttpStatus.CREATED).json(file.filename);
+  }
+
+  @Get()
+  async findAll(@Res() res: Response) {
+    const result = await this.imageUploadService.findAll();
+    return res.status(HttpStatus.OK).json(result);
   }
 }
---
+++ src/image-upload/image-upload.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { MovieRepository } from '../database/repository/movie.repository';
+
+@Injectable()
+export class ImageUploadService {
+  constructor(
+    private movieRepository: MovieRepository,
+  ) {
+  }
+
+  async findAll(): Promise<string[]> {
+    const movies = await this.movieRepository
+      .getAll()
+      .orderByDescending(x => x.popularity)
+      .include(x => x.sessions)
+      .join(x => x.sessions)
+      .where(x => x.deleted)
+      .isFalse()
+      .and(x => x.startDate)
+      .greaterThanOrEqual(new Date());
+
+    if (!movies) {
+      return [];
+    }
+
+    return movies?.map(x => x.posterURL);
+  }
+}
