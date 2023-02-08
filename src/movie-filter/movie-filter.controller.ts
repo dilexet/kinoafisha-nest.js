0c0a1bea2c7cbd04ab71cee3f3f6d6cc484446f4
Pavarha Maksim <dilexet@gmail.com>
2023-02-08 14:36:40 +0300

    added movie filters and pagination

--- src/movie-filter/movie-filter.controller.ts
+++ src/movie-filter/movie-filter.controller.ts
@@ -4,6 +4,7 @@ import { MovieFilterQueryDto } from './dto/movie-filter-query.dto';
 import { Response } from 'express';
 import { ApiTags } from '@nestjs/swagger';
 import { ApiImplicitQuery } from '@nestjs/swagger/dist/decorators/api-implicit-query.decorator';
+import { MovieFilterAfishaQueryDto } from './dto/movie-filter-afisha-query.dto';
 
 @ApiTags('Movie filter')
 @Controller('movie-filter')
@@ -11,12 +12,23 @@ export class MovieFilterController {
   constructor(private readonly movieFilterService: MovieFilterService) {
   }
 
-  @ApiImplicitQuery({ type: String, name: 'movie', required: false })
+  @ApiImplicitQuery({ type: Boolean, name: 'onlyPopular', required: false })
+  @ApiImplicitQuery({ type: Boolean, name: 'onlyFuture', required: false })
   @Get()
   async findAll(@Res() res: Response, @Query() movieFilterQuery: MovieFilterQueryDto) {
+    const result = await this.movieFilterService.findMoviesAsync(movieFilterQuery);
+    return res.status(HttpStatus.OK).json(result);
+  }
+
+  @ApiImplicitQuery({ type: String, name: 'movie', required: false })
+  @ApiImplicitQuery({ type: Number, name: 'page' })
+  @ApiImplicitQuery({ type: Number, name: 'limit' })
+  @Get('/afisha')
+  async findAllByFilters(@Res() res: Response, @Query() movieFilterQuery: MovieFilterAfishaQueryDto) {
     const result = await this.movieFilterService.findMoviesByFilterAsync(movieFilterQuery);
     return res.status(HttpStatus.OK).json(result);
   }
+
 
   @Get(':id')
   async findOne(@Res() res: Response, @Param('id') id: string) {
