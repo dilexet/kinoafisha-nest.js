f6033e2b4f84975e78ac06fa3ef8424bb37ea6a6
Pavarha Maksim <dilexet@gmail.com>
2023-02-16 13:01:55 +0300

    changes email password

--- src/mail/constants/mail-config.constants.ts
+++ src/mail/constants/mail-config.constants.ts
@@ -2,7 +2,7 @@ const mailConfigConstants = {
   EMAIL_HOST: 'smtp.gmail.com',
   EMAIL_PORT: 587,
   EMAIL_AUTH_USER: 'cinema.ticket.reservation@gmail.com',
-  EMAIL_AUTH_PASS: 'vyrbcyvzfclsqosh',
+  EMAIL_AUTH_PASS: 'jdkqteooxlogerkz',
   EMAIL_FROM: '"KinoAfisha" <cinema.ticket.reservation@gmail.com>',
   NAME_FROM: 'KinoAfisha',
 };
